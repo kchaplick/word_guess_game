afd06e523d8ce6ef616af309bc851390019cf6b2
Kasie <kchaplick92@gmail.com>
2019-02-24 16:39:32 -0500

    add js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//Create array for words that will be guessed
+var guessMe = [ "TARDIS" , "ROSE", "ANGEL", "BAD WOlF", "PONDS", "SWEETIE" ]
+
+
+//Create on key up event to display hint image and word
+
+    //Display new image
+
+    //display underscores matching word length
+
+
+//Create on key up event to display user guess 
+
+    //display letter if it matches word
+
+    //display letter if it doesn't match word
+
+    //display piece of dalek if it doesn't match
+
+//Win or loss actions
+    
+    //If user losses play Exterminate sound
+
+    //If user wins play allons y
+
+    //If user wins display next word and picture
