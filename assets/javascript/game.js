ba83eba7ab93acb3ac32ccb23f00554ce526f913
Kasie <kchaplick92@gmail.com>
2019-02-24 19:58:05 -0500

    logic to display underscores based on letters in word to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,94 @@
 //Create array for words that will be guessed
-var guessMe = [ "TARDIS" , "ROSE", "ANGEL", "BAD WOlF", "PONDS", "SWEETIE" ]
+var guessMe = [
+    {
+        name: "TARDIS",
+        url: "assets/images/tardis.jpg"
+    },
+    {
+        name: "ROSE",
+        url: "assets/images/rose.jpg"
+    },
+    {
+        name: "ANGEL",
+        url: "assets/images/angel.jpg"
+    },
+    {
+        name: "BAD WOLF",
+        url: "assets/images/badwolf.jpg"
+    },
+    {
+        name: "PONDS",
+        url: "assets/images/ponds.jpg"
+    },
+    {
+        name: "SWEETIE",
+        url: "assets/images/sweetie.jpg"
+    }
+];
 
 
-//Create on key up event to display hint image and word
-
-    //Display new image
-
-    //display underscores matching word length
+$(document).ready(function () {
 
 
-//Create on key up event to display user guess 
+    //Create an key up event to display hint image and word
+    $(document).on("click", function () {
 
-    //display letter if it matches word
 
-    //display letter if it doesn't match word
+        //Display new image
+        var hintImage = document.getElementsByClassName("hint-image")[0];
+        hintImage.setAttribute('src', guessMe[0].url);
 
-    //display piece of dalek if it doesn't match
+        //display underscores matching word length
+        //returns character length of name of object
+        var guessMeLength = guessMe[0].name.length;
+        console.log(guessMeLength);
 
-//Win or loss actions
-    
-    //If user losses play Exterminate sound
+        for (i = 0; i < guessMeLength; i++) {
+            var underscore = $("<div>" + "_" + "</div>");
+            underscore.addClass("set-underscore");
+            underscore.attr("data-letter", guessMe[0].name.charAt(i));
+            $(".word-to-guess").append(underscore);
+        }
 
-    //If user wins play allons y
+        //Create on key up event to display user guess 
+            $(document).on("keyup",function (){
+                //gets the key that is pressed
+                $(document).keydown(function(event) {
+                    var keyPressed = (String.fromCharCode(event.which));
+                
+                //if statement logic  
+                    //if keypressed is equal to letter in word replace with letter
+                    if (keyPressed === $('.set-underscore').attr("data-letter")){
+                    
+                    }
 
-    //If user wins display next word and picture
+                var userGuess = $("<div class='letters-guessed'>" + keyPressed + "</div>" );   
+                $(".wrong-guesses").append(userGuess);
+                console.log(userGuess)
+                });
+            })
+
+        //display letter if it matches word
+
+        //display letter if it doesn't match word
+
+        //display piece of dalek if it doesn't match
+
+        //Win or loss actions
+
+        //If user losses play Exterminate sound
+
+        //If user wins play allons y
+
+        //If user wins display next word and picture
+
+
+    })
+
+
+
+
+})
+
+
+
